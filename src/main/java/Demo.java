c2325eda3e59c621cea0734cb219ffb5f36d6218
Vandana <vandana@Zipcoders-MBP.fios-router.home>
2022-03-12 14:01:51 -0500

    sort String[] code works

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,7 +1,35 @@
+import java.util.Arrays;
+
 public class Demo {
 
 
-    public String demoMethod(String input){
-       return input.toUpperCase();
+    public String demoMethod ( String input ) {
+        return input.toUpperCase();
     }
+
+    public String[] stringSort ( String[] input ) {
+        for (int j = 0; j < input.length; j++) {
+
+            for (int i = 0; i < input.length - 1; i++) {
+                if(firstNumBigger(input[i], input[i+1])){
+                    String temp = input[i];
+                    input[i] = input[i + 1];
+                    input[i + 1] = temp;
+                }
+            }
+            System.out.println(Arrays.toString(input));
+
+        }
+        return input;
+    }
+
+
+
+    public Boolean  firstNumBigger(String first, String second) {
+
+         if(first.length() > second.length()) return true;
+          if(first.length()==second.length() && first.compareTo(second) >0 )  return true;
+       return false;
+    }
+
 }
--- src/test/java/DemoTest.java
+++ src/test/java/DemoTest.java
@@ -15,4 +15,46 @@ public class DemoTest {
      //then
         Assert.assertEquals(expected,actual);
     }
+
+    @Test
+    public void stringSortTest(){
+        //given
+        String[] str={"1","5","15","9","4"};
+        String[] expected={"1","4","5","9","15"};
+
+        //when
+        String[] actual=  demoObj.stringSort(str);
+
+        //then
+        Assert.assertArrayEquals(expected,actual);
+        //Assert.assertEquals(expected,actual);
+
+    }
+
+
+    @Test
+    public void firstNumBiggerTest(){
+        //given
+        String  first="15";
+        String second="10";
+         //when
+        Boolean actual=demoObj.firstNumBigger(first,second);
+
+        //then
+        Assert.assertTrue(actual);
+    }
+
+    @Test
+    public void firstNumBiggerTest1(){
+        //given
+        String  first="10";
+        String second="15";
+        //when
+        Boolean actual=demoObj.firstNumBigger(first,second);
+
+        //then
+        Assert.assertFalse(actual);
+    }
+
+
 }
